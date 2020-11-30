2621fb9054999aca7fc4dc71c8c005205c1b3412
UmeshMaharshi30 <umeshmaharshi@gmail.com>
2020-11-30 00:34:49 -0800

    feat: quitting game

--- snake-react-redux-2d/src/app/components/menus/menu.js
+++ snake-react-redux-2d/src/app/components/menus/menu.js
@@ -2,11 +2,14 @@ import React from 'react';
 import Option from './menu.option';
 
 const MenuOptions = (props) => {
+
+    const closeTab = () => window.close();
+
     return (
         <g style={{}}>
             <Option className={'menuOptions'} x={props.width/2} y={props.height/2 - 40} label={'New Game'}/>
             <Option className={'menuOptions'} x={props.width/2} y={props.height/2} label={'Instructions'}/>
-            <Option className={'menuOptions'} x={props.width/2} y={props.height/2 + 40} label={'Quit'}/>
+            <Option className={'menuOptions'} x={props.width/2} y={props.height/2 + 40} label={'Quit'} onClick={closeTab}/>
         </g>
     );
 }
--- snake-react-redux-2d/src/app/components/menus/menu.option.js
+++ snake-react-redux-2d/src/app/components/menus/menu.option.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Option = (props) => {
-    return (<text style={{textAnchor: "middle"}} {...props} onClick={() => console.log(props.label)}>{props.label} </text>)
+    return (<text style={{textAnchor: "middle"}} {...props} onClick={props.onClick}>{props.label} </text>)
 };
 
 export default Option;
